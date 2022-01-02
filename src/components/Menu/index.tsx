d71b5b3233f6ba1b5dde117370591b7bc84bb8b6
HanMoeHtet <hapo23lert@gmail.com>
2022-01-02 16:37:07 +0630

    fix: menu no result

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -111,7 +111,12 @@ const Menu: React.FC<MenuProps> = ({ restaurant }) => {
       </div>
       {filteredMenu.length === 0 ? (
         <p className="text-center text-lg mb-8">
-          No result for <span className="font-bold">{searchText}</span>
+          No result{' '}
+          {searchText && (
+            <>
+              for <span className="font-bold">{searchText}</span>
+            </>
+          )}
         </p>
       ) : (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 items-center justify-items-center gap-y-8 gap-x-8">
