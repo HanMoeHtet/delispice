1b9a9b62f16413bac0e2721ab9d47d71c7302a1c
HanMoeHtet <hapo23lert@gmail.com>
2022-01-02 14:17:05 +0630

    add: logos

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
       colors: {
         primary: '#e2293f', // red
         secondary: '#1d1c1cd9', // black
-        tertiary: 'hsl(44deg 97% 48%)', // yellow
+        tertiary: 'var(--tertiary-color)', // yellow
         'tertiary-light': 'hsl(44deg 97% 52%)', // light yellow
       },
     },
